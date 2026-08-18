562244b3d8d5e2b3e6b8be8ceb870d7e1ad7f6a5
Neuro-tune <den4ik.kozatskiy@gmail.com>
2026-08-18 11:28:46 +0300

    Adjust margin for Pure Wish Quote container

--- src/components/Screen2Gallery.tsx
+++ src/components/Screen2Gallery.tsx
@@ -329,7 +329,7 @@ export default function Screen2Gallery() {
       <div
         style={{
           maxWidth: '620px',
-          margin: '10px auto 0',
+          margin: '5px auto 0',
           textAlign: 'center',
           minHeight: '75px',
         }}
